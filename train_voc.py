f66200a30007cdbc5bdb97687aa5fef047de3588
Francisla <hzffrancesca@gmail.com>
2026-05-01 21:30:29 +0800

    关闭val的pin_memory

---
+++ train_voc.py
@@ -0,0 +1,16 @@
+from ultralytics import YOLO
+
+
+if __name__ == "__main__":
+    model = YOLO("yolov12m.yaml")
+
+    model.train(
+        data="datasets/VOC_handled/VOC_handled/VOC.yaml",
+        epochs=200,
+        imgsz=640,
+        batch=32,
+        workers=8,
+        device=0,
+        name="yolov12m_voc_baseline",
+        project="runs/voc",
+    )
