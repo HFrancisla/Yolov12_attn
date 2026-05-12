ee0ffbedf5001ccfa60ff4b8e253ae80488f6f6c
NObody <nobody@example.com>
2026-05-12 13:03:48 +0800

    更新

---
+++ train_dwt_voc_n.py
@@ -0,0 +1,16 @@
+from ultralytics import YOLO
+
+
+if __name__ == "__main__":
+    model = YOLO("yolov12n-dwt.yaml")
+
+    model.train(
+        data="datasets/VOC_handled/VOC_handled/VOC.yaml",
+        epochs=200,
+        imgsz=640,
+        batch=64,
+        workers=8,
+        device=0,
+        name="yolov12n_voc_Downsample_DWT_LL_C_to_2C_with",
+        project="runs/VOC",
+    )
